9a9c666e73eb86d41461b0c930e6fd1789c25cbf
a6cexz <lynxsm@gmail.com>
2021-02-26 21:00:18 +0300

    Test go ast traverse

--- analyzer.go
+++ analyzer.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"reflect"
 )
+
+var fileSrc = `package main
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
+`
+
+type astVisitor func(ast.Node) bool
+
+func (v astVisitor) Visit(node ast.Node) ast.Visitor {
+	if v(node) {
+		return v
+	}
+	return nil
+}
 
 func main() {
 	fmt.Println("Analyzer...")
+
+	fset := token.NewFileSet()
+
+	f, err := parser.ParseFile(fset, "main.go", fileSrc, parser.ParseComments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var bf bytes.Buffer
+	ast.Fprint(&bf, fset, f, func(string, reflect.Value) bool {
+		return true
+	})
+
+	fmt.Println(bf.String())
+
+	/*
+		fn := func(node ast.Node) bool {
+			fmt.Printf("%T\n", node)
+			return true
+		}
+		ast.Walk(astVisitor(fn), f)
+	*/
 }
